84bc32d900638ec79ec1cd56c62a21265f789afa
mirfaizanali <mir_faizan_ali@outlook.com>
2026-03-10 12:54:03 +0530

    Initial changes

---
+++ OOPS/Inheritence/Test.java
@@ -0,0 +1,23 @@
+package OOPS.Inheritence;
+
+
+
+public class Test {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.hello();
+        obj.bye();
+    }
+}
+
+class A{
+    public void hello(){
+        System.out.println("hello");
+    }
+}
+
+class B extends A{
+    public void bye(){
+        System.out.println("bye");
+    }
+}
