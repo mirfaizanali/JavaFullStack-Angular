63c5056408ad66f723ae25a7df7ddda1b176de99
mirfaizanali <mir_faizan_ali@outlook.com>
2026-03-10 12:57:39 +0530

    Second Test Changes

---
+++ OOPS/Poly/Test2.java
@@ -0,0 +1,24 @@
+package OOPS.Poly;
+
+
+
+public class Test2 {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.hello();
+
+    }
+}
+
+class A{
+    public void hello(){
+        System.out.println("hello");
+    }
+}
+
+class B extends A {
+    @Override
+    public void hello(){
+        System.out.println("bye");
+    }
+}
