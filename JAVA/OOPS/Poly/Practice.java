88df9af244aa94aa8b7ed1f48d1ca3ebf7a2d0d9
mirfaizanali <mir_faizan_ali@outlook.com>
2026-03-12 00:24:05 +0530

    Annotation Practice

--- JAVA/OOPS/Poly/Practice.java
+++ JAVA/OOPS/Poly/Practice.java
@@ -1,8 +1,22 @@
 package JAVA.OOPS.Poly;
 
+class Animal{
+    public void showtheDataWhichBewlong(){
+        System.out.println("In Animal");
+    }
+}
+
+class Cat extends Animal{
+    @Override
+    public void showtheDataWhichBewlong(){
+        System.out.println("In Cat");
+    }
+}
+
 public class Practice  {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Cat c = new Cat();
+        c.showtheDataWhichBewlong();
     }
 }
 
