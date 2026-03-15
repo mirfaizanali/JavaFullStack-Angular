ecdf3e51ca61e8919504f834ec9447bb4b398ce1
mirfaizanali <mir_faizan_ali@outlook.com>
2026-03-16 01:28:50 +0530

    Hibernate Practice

---
+++ Hibernate/HibProj/src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setsName("Ali");
+        s1.setRollNo(103);
+        s1.setsAge(26);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(org.example.Student.class);
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(s1);
+
+        transaction.commit();
+        System.out.println(s1);
+
+    }
+}
