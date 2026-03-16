c1beabcf8e0f6346ca27134461ba091f1ae0be6b
mirfaizanali <mir_faizan_ali@outlook.com>
2026-03-16 14:24:13 +0530

    Object creating Test

--- Spring Framework/Spring1/src/main/java/com/telusko/Alien.java
+++ Spring Framework/Spring1/src/main/java/com/telusko/Alien.java
@@ -2,6 +2,10 @@ package com.telusko;
 
 public class Alien {
 
+    public Alien(){
+        System.out.println("Alien Object Created");
+    }
+
 
     public void code(){
         System.out.println("Coding");
--- Spring Framework/Spring1/src/main/java/com/telusko/Hello.java
+++ Spring Framework/Spring1/src/main/java/com/telusko/Hello.java
@@ -1,7 +1,9 @@
 package com.telusko;
 
 public class Hello {
-
+    public Hello(){
+        System.out.println("Hello Object Created");
+    }
     public void  sayHello()
     {
         System.out.println("Hello World");
