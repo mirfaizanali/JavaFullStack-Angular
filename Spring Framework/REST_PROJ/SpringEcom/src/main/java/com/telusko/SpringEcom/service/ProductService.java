37576594c0b5fb8b5c5629ef1384dcadbc0308de
mirfaizanali <mir_faizan_ali@hotmail.com>
2026-04-04 13:54:39 +0530

    Ecom application

--- Spring Framework/REST_PROJ/SpringEcom/src/main/java/com/telusko/SpringEcom/service/ProductService.java
+++ Spring Framework/REST_PROJ/SpringEcom/src/main/java/com/telusko/SpringEcom/service/ProductService.java
@@ -4,7 +4,9 @@ import com.telusko.SpringEcom.model.Product;
 import com.telusko.SpringEcom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -15,5 +17,17 @@ public class ProductService {
 
     public List<Product>getAllProducts(){
         return productRepo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepo.findById(id).get();
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return  productRepo.save(product);
     }
 }
